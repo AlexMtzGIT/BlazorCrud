0445725c376920e6b3021851888d9bee743c0e44
alejandro.martinezal <alejandro.martinezal@CMMXLP-01583>
2026-02-26 15:28:11 -0600

    Creacion de ApplicationDbContext

---
+++ BlazorCrud/Components/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BlazorCrud.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace BlazorCrud.Components.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+        
+        }
+
+        //Colocamos cada uno de los modelos
+        public DbSet<Libro> Libro { get; set; }
+    }
+}
