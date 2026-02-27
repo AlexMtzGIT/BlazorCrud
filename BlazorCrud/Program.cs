232cf46c9a1e6f9776949b55c648da30a0c613d1
alejandro.martinezal <alejandro.martinezal@CMMXLP-01583>
2026-02-27 14:42:34 -0600

    Creacion Cadena de conexion con base de datos (MSSQLLocalDB)

--- BlazorCrud/Program.cs
+++ BlazorCrud/Program.cs
@@ -1,6 +1,12 @@
 using BlazorCrud.Components;
+using BlazorCrud.Components.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Configurar conexión a MSSQLLocalDB (Microsoft SQL Local Data Base) (En este caso usamos MSSQLLocalDB, osea base de datos de microsoft, pero lo normal es usar una de sql server con usuario y constraseña, las dos se pueden acceder desde sql server management studio)
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSQLLocalDB")));
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
